ed896c4099070cd62baaf0dce01323a6cbb151a4
archiecarrot123 <archie.halliwell@gmail.com>
2020-06-27 18:05:26 +1000

    added game

---
+++ game.py
@@ -0,0 +1,37 @@
+import pygame
+
+def attackOrCrouch(creature, enemyHealth):
+    while enemyHealth > 0:
+        print("A " + creature + " attacks!")
+        print(creature + "'s Health " + str(enemyHealth))
+        print(name + "'s Health " + "placeholder")
+        print("Attack, or Crouch?")
+        action = input("(a/c)? ")
+        if action.lower() == "a":
+            enemyHealth = enemyHealth - 3
+        elif action.lower() == "c":
+            print("error: not implemented yet")
+        else:
+            print("What?")
+    if enemyHealth <= 0:
+        print("You beat the " + creature)
+        return(True)
+    else:
+        print("ERROR IN HEALTH CALCULATION" + enemyHealth)
+        return(False)
+
+name = input("What is your name? ")
+print("Hello " + name)
+print("Random mode, or predictable mode?")
+mode = input("(R/p)? ")
+if mode.lower() == "p":
+    print("Predictable")
+    p = True
+else:
+    print("Random")
+    p = False
+print('Tutorial')
+if attackOrCrouch("goblin", 10) == True:
+    print("You win. This is the end of this <40 line game")
+else:
+    print("You lose. How?")
